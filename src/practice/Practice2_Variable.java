c187fddbadd34e322359023403c53b32311ee548
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-04-18 22:11:25 +0900

    Practice2 Variable

---
+++ src/practice/Practice2_Variable.java
@@ -0,0 +1,18 @@
+package practice;
+
+public class Practice2_Variable {
+	
+	public static void main(String[] args) {
+		// 宣言する		
+		int month;
+		// 代入する	
+		month = 9;
+		// 初期化する	
+		int day = 16;
+		
+		System.out.println("このチャンネルは、");
+		// 参照する	
+		System.out.println(month + "月" + day + "日に公開しました！");
+	}
+
+}
