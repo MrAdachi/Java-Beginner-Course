fb6632b5200ec1d40d79ad5a323ba5117b81e34a
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-04-18 23:46:01 +0900

    Practice4 Array3

---
+++ src/practice/Practice4_Array3.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class Practice4_Array3 {
+
+	public static void main(String[] args) {
+		/*	コマンドラインから入力したデータはargs配列に
+			入った状態でプログラムがスタートする
+		 */
+		System.out.println("args[0]：" + args[0]);
+		System.out.println("args[1]：" + args[1]);
+		System.out.println("args.length：" + args.length);
+	}
+
+}
