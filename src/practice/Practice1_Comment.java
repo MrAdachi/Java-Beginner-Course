0b5032a4561a20822b99655a40940de0a8a050d1
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-04-18 11:10:21 +0900

    Practice1 Write Comment

---
+++ src/practice/Practice1_Comment.java
@@ -0,0 +1,12 @@
+/* Practice1_Comment.java
+   文字列を表示するプログラム
+   2024.4.18　足立作成　　　　　 */
+
+package practice;
+
+public class Practice1_Comment {
+	public static void main(String[] args) {
+		System.out.println("Javaを楽しもう!");  //画面表示
+	}
+
+}
