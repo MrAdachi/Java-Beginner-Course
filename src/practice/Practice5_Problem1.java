5ebc3e8c36eeb9ebe672f5b7790ae180f7ff0a4d
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-04-19 21:38:28 +0900

    Practice5_Problem1＆2

---
+++ src/practice/Practice5_Problem1.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class Practice5_Problem1 {
+
+	public static void main(String[] args) {
+		System.out.println("こんにちは。");
+		System.out.println("一緒にがんばりましょう！");
+
+	}
+
+}
---
+++ src/practice/Practice5_Problem2.java
@@ -0,0 +1,21 @@
+package practice;
+
+public class Practice5_Problem2 {
+
+	public static void main(String[] args) {
+		// 変数の宣言と値の代入
+		int num = 10;
+		double pi = 3.14;
+		char blood = 'A';
+		boolean fun = true;
+		String msg = "一緒にがんばりましょう！";
+				
+		//	値の表示
+		System.out.println("num：" + num);
+		System.out.println("pi：" + pi);
+		System.out.println("blood：" + blood);
+		System.out.println("fun：" + fun);
+		System.out.println("msg：" + msg);	
+	}
+
+}
