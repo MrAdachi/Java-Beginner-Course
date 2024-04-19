dde5d052550abaff6f0f0415898efa40d92cd783
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-04-20 00:16:42 +0900

    Practice9 Loop1

---
+++ src/practice/Practice9_Loop1.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class Practice9_Loop1 {
+	public static void main(String[] args) {
+		int i = 0;
+		while(i < 3) {
+			System.out.println("i = " + i);
+			i++;
+		}	
+	}
+}
