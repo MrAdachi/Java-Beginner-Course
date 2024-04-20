3c8b9b6fc23cffa746a0b987d41939176657fd8d
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-04-20 16:16:38 +0900

    Practice10 Problem4

---
+++ src/practice/Practice10_Problem4.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class Practice10_Problem4 {
+
+	public static void main(String[] args) {
+		//	メッセージを10回表示(while文)
+		int i = 1;
+		
+		while(i <= 10) {
+			System.out.println( i + "回目：フリーラーニングを広げたい！");
+			i ++;
+		}
+	}
+
+}
