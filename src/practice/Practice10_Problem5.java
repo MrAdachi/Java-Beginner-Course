e4db6a40ff9004d9f9cc269f1203e383e9745994
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-04-20 16:25:59 +0900

    Practice10 Problem5

---
+++ src/practice/Practice10_Problem5.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class Practice10_Problem5 {
+
+	public static void main(String[] args) {
+		//	コマンドラインの入力文字列を表示（for文）
+		for (int i = 0; i < args.length; i++) {
+			System.out.println(args[i]);
+		}
+	}
+
+}
